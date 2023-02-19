1498f4d565160127292acd9cc56841b2985fc106
Luka Vashakmadze <50746021+Vashakmadze@users.noreply.github.com>
2023-02-19 21:49:28 +0100

    changed links

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ const App = () => {
     <BrowserRouter>
       <header className='w-full flex justify-between items-center
     bg-white sm:px-8 px-4 border-b border-b-[#e6ebf4]'>
-        <Link to='/'>
+        <Link to='/ai-image-generation/'>
           <img src={logo} alt='Logo' className="w-28 object-contain" />
         </Link>
-        <Link to='/create-post' className='font-inter font-medium
+        <Link to='/ai-image-generation/create-post' className='font-inter font-medium
         bg-[#6469ff] text-white py-2 px-4 rounded-md'>
         Create
         </Link>
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(180vh-73px)]'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/ai-image-generation/" element={<Home />} />
+          <Route path="/ai-image-generation/create-post" element={<CreatePost />} />
         </Routes>
       </main>
     </BrowserRouter>
